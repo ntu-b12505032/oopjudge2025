b7ac126b9694d40d4d7e9daf598744a8e50274c8
ntu-b12505032 <b12505032@ntu.edu.tw>
2025-05-19 00:36:04 +0800

    Update Triangle.java

--- 8/Triangle.java
+++ 8/Triangle.java
@@ -1,28 +1,43 @@
 /**
- * Represents a regular triangle.
- * Provides implementation for area and perimeter calculation.
+ * Represents an equilateral triangle.
+ * Extends the abstract Shape class and implements area and perimeter calculation.
  */
 public class Triangle extends Shape {
 
     /**
-     * Constructs a Triangle with given side length.
+     * Constructs a Triangle with the specified side length.
      * 
-     * @param length the side length
+     * @param length the side length of the triangle
      */
     public Triangle(double length) {
         super(length);
     }
 
+    /**
+     * Sets the side length of the triangle.
+     * 
+     * @param length the new side length
+     */
     @Override
     public void setLength(double length) {
         this.length = length;
     }
 
+    /**
+     * Calculates the area of the equilateral triangle.
+     * 
+     * @return the area
+     */
     @Override
     public double getArea() {
         return (Math.sqrt(3) / 4) * length * length;
     }
 
+    /**
+     * Calculates the perimeter of the triangle.
+     * 
+     * @return the perimeter
+     */
     @Override
     public double getPerimeter() {
         return 3 * length;
