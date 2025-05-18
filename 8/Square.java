97bb0282c726942e9044dcfe6628de1aea1e0245
ntu-b12505032 <b12505032@ntu.edu.tw>
2025-05-19 00:36:02 +0800

    Update Square.java

--- 8/Square.java
+++ 8/Square.java
@@ -1,28 +1,44 @@
 /**
  * Represents a square shape.
- * Provides implementation for area and perimeter calculation.
+ * Extends the abstract Shape class and implements area and perimeter calculation.
  */
 public class Square extends Shape {
 
     /**
-     * Constructs a Square with given side length.
+     * Constructs a Square with the specified side length.
      * 
-     * @param length the side length
+     * @param length the side length of the square
      */
+    
     public Square(double length) {
         super(length);
     }
 
+    /**
+     * Sets the side length of the square.
+     * 
+     * @param length the new side length
+     */
     @Override
     public void setLength(double length) {
         this.length = length;
     }
 
+    /**
+     * Calculates the area of the square.
+     * 
+     * @return the area
+     */
     @Override
     public double getArea() {
         return length * length;
     }
 
+    /**
+     * Calculates the perimeter of the square.
+     * 
+     * @return the perimeter
+     */
     @Override
     public double getPerimeter() {
         return 4 * length;
