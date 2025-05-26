281b25664ee6b04ae332cdde518c83415ee9e296
ntu-b12505032 <b12505032@ntu.edu.tw>
2025-05-26 13:07:37 +0800

    Create UnknownCmdException.java

---
+++ 9/UnknownCmdException.java
@@ -0,0 +1,11 @@
+public class UnknownCmdException extends Exception{
+    public UnknownCmdException(){
+        super();
+    }
+
+    public UnknownCmdException(String str){
+        super(str);
+    }
+
+
+}
