29a6c2186c21b2ae45468123d6aaa0873405084f
ntu-b12505032 <b12505032@ntu.edu.tw>
2025-06-07 15:55:12 +0800

    Update UnknownCmdException.java

--- 9/UnknownCmdException.java
+++ 9/UnknownCmdException.java
@@ -1,11 +1,14 @@
-public class UnknownCmdException extends Exception{
-    public UnknownCmdException(){
-        super();
+/**
+ * Custom exception for handling unknown commands in the calculator.
+ */
+public class UnknownCmdException extends Exception {
+
+    /**
+     * Constructs an UnknownCmdException with a specified error message.
+     *
+     * @param errMessage the detail message describing the exception.
+     */
+    public UnknownCmdException(String errMessage) {
+        super(errMessage);
     }
-
-    public UnknownCmdException(String str){
-        super(str);
-    }
-
-
 }
